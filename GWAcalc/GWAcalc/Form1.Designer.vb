ba7ed8f472a22d9095e7944eb26ed77d790a9dbe
gmaljon <gmaljongm@gmail.com>
2016-04-01 09:14:19 -0700

    Added files via upload

---
+++ GWAcalc/GWAcalc/Form1.Designer.vb
@@ -0,0 +1,389 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.u1 = New System.Windows.Forms.ComboBox()
+        Me.g1 = New System.Windows.Forms.ComboBox()
+        Me.u2 = New System.Windows.Forms.ComboBox()
+        Me.g2 = New System.Windows.Forms.ComboBox()
+        Me.u3 = New System.Windows.Forms.ComboBox()
+        Me.g3 = New System.Windows.Forms.ComboBox()
+        Me.u4 = New System.Windows.Forms.ComboBox()
+        Me.g4 = New System.Windows.Forms.ComboBox()
+        Me.u5 = New System.Windows.Forms.ComboBox()
+        Me.g5 = New System.Windows.Forms.ComboBox()
+        Me.u6 = New System.Windows.Forms.ComboBox()
+        Me.u7 = New System.Windows.Forms.ComboBox()
+        Me.u8 = New System.Windows.Forms.ComboBox()
+        Me.g6 = New System.Windows.Forms.ComboBox()
+        Me.g7 = New System.Windows.Forms.ComboBox()
+        Me.g8 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblGWA = New System.Windows.Forms.Label()
+        Me.g9 = New System.Windows.Forms.ComboBox()
+        Me.u9 = New System.Windows.Forms.ComboBox()
+        Me.g10 = New System.Windows.Forms.ComboBox()
+        Me.u10 = New System.Windows.Forms.ComboBox()
+        Me.btnCompute = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.unitPanel = New System.Windows.Forms.Panel()
+        Me.gradePanel = New System.Windows.Forms.Panel()
+        Me.unitPanel.SuspendLayout()
+        Me.gradePanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'u1
+        '
+        Me.u1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u1.FormattingEnabled = True
+        Me.u1.Location = New System.Drawing.Point(3, 21)
+        Me.u1.Name = "u1"
+        Me.u1.Size = New System.Drawing.Size(55, 21)
+        Me.u1.TabIndex = 0
+        '
+        'g1
+        '
+        Me.g1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g1.FormattingEnabled = True
+        Me.g1.Location = New System.Drawing.Point(3, 19)
+        Me.g1.Name = "g1"
+        Me.g1.Size = New System.Drawing.Size(55, 21)
+        Me.g1.TabIndex = 1
+        '
+        'u2
+        '
+        Me.u2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u2.FormattingEnabled = True
+        Me.u2.Location = New System.Drawing.Point(3, 47)
+        Me.u2.Name = "u2"
+        Me.u2.Size = New System.Drawing.Size(55, 21)
+        Me.u2.TabIndex = 2
+        '
+        'g2
+        '
+        Me.g2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g2.FormattingEnabled = True
+        Me.g2.Location = New System.Drawing.Point(3, 45)
+        Me.g2.Name = "g2"
+        Me.g2.Size = New System.Drawing.Size(55, 21)
+        Me.g2.TabIndex = 3
+        '
+        'u3
+        '
+        Me.u3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u3.FormattingEnabled = True
+        Me.u3.Location = New System.Drawing.Point(3, 73)
+        Me.u3.Name = "u3"
+        Me.u3.Size = New System.Drawing.Size(55, 21)
+        Me.u3.TabIndex = 4
+        '
+        'g3
+        '
+        Me.g3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g3.FormattingEnabled = True
+        Me.g3.Location = New System.Drawing.Point(3, 71)
+        Me.g3.Name = "g3"
+        Me.g3.Size = New System.Drawing.Size(55, 21)
+        Me.g3.TabIndex = 5
+        '
+        'u4
+        '
+        Me.u4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u4.FormattingEnabled = True
+        Me.u4.Location = New System.Drawing.Point(3, 99)
+        Me.u4.Name = "u4"
+        Me.u4.Size = New System.Drawing.Size(55, 21)
+        Me.u4.TabIndex = 6
+        '
+        'g4
+        '
+        Me.g4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g4.FormattingEnabled = True
+        Me.g4.Location = New System.Drawing.Point(3, 97)
+        Me.g4.Name = "g4"
+        Me.g4.Size = New System.Drawing.Size(55, 21)
+        Me.g4.TabIndex = 7
+        '
+        'u5
+        '
+        Me.u5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u5.FormattingEnabled = True
+        Me.u5.Location = New System.Drawing.Point(3, 125)
+        Me.u5.Name = "u5"
+        Me.u5.Size = New System.Drawing.Size(55, 21)
+        Me.u5.TabIndex = 8
+        '
+        'g5
+        '
+        Me.g5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g5.FormattingEnabled = True
+        Me.g5.Location = New System.Drawing.Point(3, 123)
+        Me.g5.Name = "g5"
+        Me.g5.Size = New System.Drawing.Size(55, 21)
+        Me.g5.TabIndex = 9
+        '
+        'u6
+        '
+        Me.u6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u6.FormattingEnabled = True
+        Me.u6.Location = New System.Drawing.Point(3, 151)
+        Me.u6.Name = "u6"
+        Me.u6.Size = New System.Drawing.Size(55, 21)
+        Me.u6.TabIndex = 10
+        '
+        'u7
+        '
+        Me.u7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u7.FormattingEnabled = True
+        Me.u7.Location = New System.Drawing.Point(3, 177)
+        Me.u7.Name = "u7"
+        Me.u7.Size = New System.Drawing.Size(55, 21)
+        Me.u7.TabIndex = 11
+        '
+        'u8
+        '
+        Me.u8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u8.FormattingEnabled = True
+        Me.u8.Location = New System.Drawing.Point(3, 203)
+        Me.u8.Name = "u8"
+        Me.u8.Size = New System.Drawing.Size(55, 21)
+        Me.u8.TabIndex = 12
+        '
+        'g6
+        '
+        Me.g6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g6.FormattingEnabled = True
+        Me.g6.Location = New System.Drawing.Point(3, 149)
+        Me.g6.Name = "g6"
+        Me.g6.Size = New System.Drawing.Size(55, 21)
+        Me.g6.TabIndex = 13
+        '
+        'g7
+        '
+        Me.g7.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g7.FormattingEnabled = True
+        Me.g7.Location = New System.Drawing.Point(3, 175)
+        Me.g7.Name = "g7"
+        Me.g7.Size = New System.Drawing.Size(55, 21)
+        Me.g7.TabIndex = 14
+        '
+        'g8
+        '
+        Me.g8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g8.FormattingEnabled = True
+        Me.g8.Location = New System.Drawing.Point(3, 201)
+        Me.g8.Name = "g8"
+        Me.g8.Size = New System.Drawing.Size(55, 21)
+        Me.g8.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "UNITS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "GRADE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(166, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "GWA"
+        '
+        'lblGWA
+        '
+        Me.lblGWA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGWA.Location = New System.Drawing.Point(147, 32)
+        Me.lblGWA.Name = "lblGWA"
+        Me.lblGWA.Size = New System.Drawing.Size(69, 21)
+        Me.lblGWA.TabIndex = 19
+        '
+        'g9
+        '
+        Me.g9.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g9.FormattingEnabled = True
+        Me.g9.Location = New System.Drawing.Point(3, 227)
+        Me.g9.Name = "g9"
+        Me.g9.Size = New System.Drawing.Size(55, 21)
+        Me.g9.TabIndex = 21
+        '
+        'u9
+        '
+        Me.u9.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u9.FormattingEnabled = True
+        Me.u9.Location = New System.Drawing.Point(3, 229)
+        Me.u9.Name = "u9"
+        Me.u9.Size = New System.Drawing.Size(55, 21)
+        Me.u9.TabIndex = 20
+        '
+        'g10
+        '
+        Me.g10.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.g10.FormattingEnabled = True
+        Me.g10.Location = New System.Drawing.Point(3, 253)
+        Me.g10.Name = "g10"
+        Me.g10.Size = New System.Drawing.Size(55, 21)
+        Me.g10.TabIndex = 23
+        '
+        'u10
+        '
+        Me.u10.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.u10.FormattingEnabled = True
+        Me.u10.Location = New System.Drawing.Point(3, 255)
+        Me.u10.Name = "u10"
+        Me.u10.Size = New System.Drawing.Size(55, 21)
+        Me.u10.TabIndex = 22
+        '
+        'btnCompute
+        '
+        Me.btnCompute.Location = New System.Drawing.Point(147, 59)
+        Me.btnCompute.Name = "btnCompute"
+        Me.btnCompute.Size = New System.Drawing.Size(69, 23)
+        Me.btnCompute.TabIndex = 24
+        Me.btnCompute.Text = "Compute"
+        Me.btnCompute.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(147, 86)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(69, 23)
+        Me.btnReset.TabIndex = 25
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(147, 113)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(69, 23)
+        Me.btnExit.TabIndex = 26
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'unitPanel
+        '
+        Me.unitPanel.Controls.Add(Me.u10)
+        Me.unitPanel.Controls.Add(Me.u9)
+        Me.unitPanel.Controls.Add(Me.u8)
+        Me.unitPanel.Controls.Add(Me.u7)
+        Me.unitPanel.Controls.Add(Me.u6)
+        Me.unitPanel.Controls.Add(Me.u5)
+        Me.unitPanel.Controls.Add(Me.u4)
+        Me.unitPanel.Controls.Add(Me.u3)
+        Me.unitPanel.Controls.Add(Me.u2)
+        Me.unitPanel.Controls.Add(Me.u1)
+        Me.unitPanel.Location = New System.Drawing.Point(7, 11)
+        Me.unitPanel.Name = "unitPanel"
+        Me.unitPanel.Size = New System.Drawing.Size(65, 292)
+        Me.unitPanel.TabIndex = 27
+        '
+        'gradePanel
+        '
+        Me.gradePanel.Controls.Add(Me.g10)
+        Me.gradePanel.Controls.Add(Me.g9)
+        Me.gradePanel.Controls.Add(Me.g8)
+        Me.gradePanel.Controls.Add(Me.g7)
+        Me.gradePanel.Controls.Add(Me.g6)
+        Me.gradePanel.Controls.Add(Me.g5)
+        Me.gradePanel.Controls.Add(Me.g4)
+        Me.gradePanel.Controls.Add(Me.g3)
+        Me.gradePanel.Controls.Add(Me.g2)
+        Me.gradePanel.Controls.Add(Me.g1)
+        Me.gradePanel.Location = New System.Drawing.Point(75, 13)
+        Me.gradePanel.Name = "gradePanel"
+        Me.gradePanel.Size = New System.Drawing.Size(66, 290)
+        Me.gradePanel.TabIndex = 28
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(222, 303)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.gradePanel)
+        Me.Controls.Add(Me.unitPanel)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnCompute)
+        Me.Controls.Add(Me.lblGWA)
+        Me.Controls.Add(Me.Label3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GWA Calculator"
+        Me.unitPanel.ResumeLayout(False)
+        Me.gradePanel.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents u1 As ComboBox
+    Friend WithEvents g1 As ComboBox
+    Friend WithEvents u2 As ComboBox
+    Friend WithEvents g2 As ComboBox
+    Friend WithEvents u3 As ComboBox
+    Friend WithEvents g3 As ComboBox
+    Friend WithEvents u4 As ComboBox
+    Friend WithEvents g4 As ComboBox
+    Friend WithEvents u5 As ComboBox
+    Friend WithEvents g5 As ComboBox
+    Friend WithEvents u6 As ComboBox
+    Friend WithEvents u7 As ComboBox
+    Friend WithEvents u8 As ComboBox
+    Friend WithEvents g6 As ComboBox
+    Friend WithEvents g7 As ComboBox
+    Friend WithEvents g8 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblGWA As Label
+    Friend WithEvents g9 As ComboBox
+    Friend WithEvents u9 As ComboBox
+    Friend WithEvents g10 As ComboBox
+    Friend WithEvents u10 As ComboBox
+    Friend WithEvents btnCompute As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents unitPanel As Panel
+    Friend WithEvents gradePanel As Panel
+End Class
---
+++ GWAcalc/GWAcalc/Form1.vb
@@ -0,0 +1,103 @@
+﻿Public Class Form1
+
+    Dim grades() As Double = {0, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0}
+    Dim units() As Double = {0, 2.0, 3.0, 4.0, 5.0}
+    Dim gradeList(9) As Double
+    Dim unitList(9) As Double
+
+    Public Sub FillComboBoxes()
+
+        For Each unitBox As ComboBox In unitPanel.Controls
+            AssignArray(unitBox, 1)
+            unitBox.SelectedIndex = 0
+        Next
+
+        For Each gradeBox As ComboBox In gradePanel.Controls
+            AssignArray(gradeBox, 2)
+            gradeBox.SelectedIndex = 0
+        Next
+
+    End Sub
+
+    Public Sub AssignArray(cmb As ComboBox, category As Integer)
+
+        If category = 1 Then
+            For n = 0 To 4
+                cmb.Items.Add(units(n))
+            Next
+        Else
+            For n = 0 To 9
+                cmb.Items.Add(grades(n))
+            Next
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load() Handles MyBase.Load
+        FillComboBoxes()
+    End Sub
+
+    Private Sub Button3_Click() Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub Button2_Click() Handles btnReset.Click
+        ResetComboBoxes()
+        lblGWA.Text = ""
+
+    End Sub
+
+    Public Sub ResetComboBoxes()
+
+        For Each unitBox As ComboBox In unitPanel.Controls
+            unitBox.SelectedIndex = 0
+        Next
+
+        For Each gradeBox As ComboBox In gradePanel.Controls
+            gradeBox.SelectedIndex = 0
+        Next
+
+    End Sub
+
+    Private Sub Button1_Click() Handles btnCompute.Click
+
+        PopulateArray()
+
+        Dim totalUnits As Double = 0
+        Dim unitXgrade As Double = 0
+        Dim gwa As Double = 0
+
+        For idx = 0 To 9
+            totalUnits += unitList(idx)
+        Next
+
+        For idx = 0 To 9
+            unitXgrade += (unitList(idx) * gradeList(idx))
+        Next
+
+        gwa = unitXgrade / totalUnits
+
+        lblGWA.Text = gwa.ToString("N2")
+
+    End Sub
+
+    Public Sub PopulateArray()
+
+        Dim idx As Integer = 0
+        Dim idy As Integer = 0
+
+        For Each unitBox As ComboBox In unitPanel.Controls
+            unitList(idx) = Convert.ToDouble(unitBox.Text)
+            idx += 1
+        Next
+
+        For Each gradeBox As ComboBox In gradePanel.Controls
+            gradeList(idy) = Convert.ToDouble(gradeBox.Text)
+            idy += 1
+        Next
+
+    End Sub
+
+
+
+End Class
